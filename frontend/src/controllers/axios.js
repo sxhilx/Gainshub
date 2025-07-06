c9817ba326f177a833a94c944d2829dd9114cb4e
sxhilx <jadasahil786@gmail.com>
2025-07-06 15:30:39 +0200

    update: changed baseURL with live API url

--- frontend/src/controllers/axios.js
+++ frontend/src/controllers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000/api/v1'
+    baseURL: 'https://gainshub.onrender.com/api/v1'
 })
 
 API.interceptors.request.use((req) => {
